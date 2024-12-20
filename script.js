9dad01346b55b05c353e4a119aaee7d35939a2f5
LuciaWeiner <148787097+LuciaWeiner@users.noreply.github.com>
2024-12-20 10:41:44 -0500

    Name typing animation

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const nameElement = document.querySelector('.typing-text');
+const nameText = "Lucia Weiner";
+let charIndex = 0;
+
+function typeAnimation(){
+    if (charIndex < nameText.length) {
+        nameElement.textContent += nameText.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeAnimation, 200); // Adjust typing speed (ms per character)
+    } else {
+        setTimeout(() =>{
+            nameElement.textContent = "";
+            charIndex = 0;
+            typeAnimation();
+        }, 5000);
+    }
+}
+
+typeAnimation();
